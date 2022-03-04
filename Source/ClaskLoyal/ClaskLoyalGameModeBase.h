ed10dc57f42cb22b9c50aacd996115e4b5a032cd
Roshan Govind Bellary <roshangovind96@gmail.com>
2022-03-03 20:51:33 -0500

    Initial commit

---
+++ Source/ClaskLoyal/ClaskLoyalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ClaskLoyalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLASKLOYAL_API AClaskLoyalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
